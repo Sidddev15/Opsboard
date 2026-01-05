e1987379e42dd80dbe8ff8d55cecc745345a0644
sidddev15 <siddharthpersonal1506@gmail.com>
2026-01-05 23:10:44 +0530

    chore: added vercel analytics on the opsboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import BoardPage from './pages/BoardPage';
 import { AuthProvider, useAuth } from './state/auth';
+import { Analytics } from '@vercel/analytics/react';
 
 function Protected({ children }: { children: React.ReactNode }) {
   const { token, isLoading } = useAuth();
@@ -12,19 +13,22 @@ function Protected({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <Protected>
-              <BoardPage />
-            </Protected>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <Protected>
+                <BoardPage />
+              </Protected>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AuthProvider>
+      <Analytics />
+    </>
   );
 }
